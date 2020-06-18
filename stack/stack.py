4ccea04343bc307fb3e61987af11596178dd6d01
durbcodes <nick.durbin@gmail.com>
2020-06-18 10:28:05 -0400

    Got the two variations of array and LinkedList working/passing.

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,7 @@
+import sys 
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +14,42 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.insert(0, value)
+
+#     def pop(self):
+#         if len(self.storage) >= 1:
+#             val = self.storage[0]
+#             self.storage.pop(0)
+#             return val
+#         else:
+#             return None
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size = self.size + 1
+        return value
 
     def pop(self):
-        pass
+        if self.size >= 1:
+            val = self.storage.remove_head()
+            self.size = self.size - 1
+            return val
+        else:
+            return None
